423ac069669d946aada535ed8882c22e1ba804da
maria-bd-designs <mariabddesigns@gmail.com>
2025-04-12 00:48:05 +0100

    Your commit message here

--- beckend/ai_model/ml.py
+++ beckend/ai_model/ml.py
@@ -9,18 +9,19 @@ def load_rbs_model():
     global rbs_model_instance
     if rbs_model_instance is None:
         rbs_model_instance = xgb.XGBRegressor()
-        rbs_model_instance.load_model("/home/rebbouh/Robtics/beckend/ai_model/models/xgb_rbs_model.json")
+        rbs_model_instance.load_model("C:\\mobilis-sites-management\\beck-end\\beckend\\ai_model\\models\\xgb_rbs_model.json")
     return rbs_model_instance
 
 def load_freq_model():
     global freq_model_instance
     if freq_model_instance is None:
         freq_model_instance = xgb.XGBRegressor()
-        freq_model_instance.load_model("/home/rebbouh/Robtics/beckend/ai_model/models/xgb_freq_model.json")
+        freq_model_instance.load_model("C:\\mobilis-sites-management\\beck-end\\beckend\\ai_model\\models\\xgb_freq_model.json")
     return freq_model_instance
 
 def load_rl_model():
     global rl_model
     if rl_model is None:
-        rl_model = PPO.load("/home/rebbouh/Robtics/beckend/ai_model/models/dc_cooling_ppo.zip")  # Update class/path if needed
+        rl_model = PPO.load("C:\\mobilis-sites-management\\beck-end\\beckend\\ai_model\\models\\dc_cooling_ppo.zip")  
+        # Update class/path if needed
     return rl_model
